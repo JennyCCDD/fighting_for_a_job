7e1124432ff65366db2263c0f7b2d975b04e95ac
JennyCCDD <18874821065@163.com>
2021-01-09 11:38:12 +0800

    LC20210106_v2

--- LC 两个数组的交集II.py
+++ LC 两个数组的交集II.py
@@ -6,6 +6,7 @@
     https://leetcode-cn.com/problems/single-number/
 @revise log:
     2021.01.06 创建程序
+    2021.01.09 运行成功
 """
 class Solution(object):
     def intersect(self, nums1, nums2):
@@ -14,8 +15,16 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
-
+        both = set(nums1) & set(nums2)
+        both_count = []
+        for item in list(both):
+            count = min((nums1.count(item)), (nums2.count(item)))
+            ###############################
+            both_count += [item]* count
+            ###############################
+        return both_count
 
 solution = Solution()
-result = solution.intersect([1,2,2,1],[2,2])
+# result = solution.intersect([1,2,2,1],[2,2])
+result = solution.intersect([4,9,5],[9,4,9,8,4])
 print(result)
