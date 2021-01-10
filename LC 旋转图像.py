82d58feb3002d3d450f67698065921c26408fdd4
JennyCCDD <18874821065@163.com>
2021-01-10 13:49:47 +0800

    LC20210110

--- LC 旋转图像.py
+++ LC 旋转图像.py
@@ -3,10 +3,14 @@
 @author: Mengxuan Chen
 @description:
     旋转图像
+    https://leetcode-cn.com/problems/rotate-image/
 
 @revise log:
     2021.01.09 创建程序
-                解题思路：
+                解题思路：原地旋转，使用临时变量temp防止原地旋转被覆盖
+                时间复杂度：O(N^2)O(N 2 )，其中 NN 是 \textit{matrix}matrix 的边长。我们需要枚举的子矩阵大小为 O(\lfloor n/2 \rfloor \times \lfloor (n+1)/2 \rfloor) = O(N^2)⌊n/2⌋×⌊(n+1)/2⌋)=O(N 2)。
+                空间复杂度：O(1)O(1)。为原地旋转。
+
 """
 class Solution(object):
     def rotate(self, matrix):
@@ -14,25 +18,17 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
-        # matrix_ = matrix.copy()
-        # matrix_[0][2] = matrix[0][0]
-        # matrix_[0][0] = matrix[2][0]
-        # matrix_[2][0] = matrix[2][2]
-        # matrix_[2][2] = matrix[0][2]
-        # matrix_[0][1] = matrix[1][0]
-        # matrix_[1][0] = matrix[2][1]
-        # matrix_[2][1] = matrix[1][2]
-        # matrix_[1][2] = matrix[0][1]
-        # row = len(matrix)
-        # for i in range(row):
-        #     for j in range(row):
-        #         matrix[row - i - 1][row - j - 1] = matrix[i][j]
+        row = len(matrix)
+        for i in range(int(row/2)):
+            for j in range(int((row+1)/2)):
+                temp = matrix[i][j]
+                matrix[i][j]=matrix[row-j-1][i]
+                matrix[row-j-1][i]=matrix[row-i-1][row-j-1]
+                matrix[row-i-1][row-j-1]=matrix[j][row-i-1]
+                matrix[j][row-i-1]=temp
+
         return matrix
-# [
-#   [7,4,1],
-#   [8,5,2],
-#   [9,6,3]
-# ]
+
 solution = Solution()
 result = solution.rotate([
   [1,2,3],
@@ -40,4 +36,3 @@ result = solution.rotate([
   [7,8,9]
 ])
 print(result)
-
