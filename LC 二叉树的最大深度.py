96967065ba6193d665e8a199e1380e14d6388d86
JennyCCDD <18874821065@163.com>
2021-01-22 23:38:49 +0800

    LC20210122

---
+++ LC 二叉树的最大深度.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Mengxuan Chen
+@description:
+    二叉树的最大深度
+@revise log:
+    2021.01.22 创建程序
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
