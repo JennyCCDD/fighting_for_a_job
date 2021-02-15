c549a8fc81257c3183e8f1e3571009af2a1d30e2
JennyCCDD <18874821065@163.com>
2021-02-15 19:50:59 +0800

    LC20210215

--- LC 最大子序和.py
+++ LC 最大子序和.py
@@ -14,18 +14,27 @@ class Solution(object):
         :rtype: int
         """
         n = len(nums)
-        if n == 0: return 0  # 边界条件
-        if n == 1: return nums[0]
-        dp = [0] * n
+        pre = 0
+        maxnums = nums[0]
+        for i in nums:
+            pre = max(pre + i, i)
+            maxnums = max(maxnums, pre)
+        return maxnums
 
-        for i in range(1, n):
-            ddp = [0] * i
-            for j in range(1,i):
-                ddp[j] = sum(nums[i-j:i])
-            dp[i] = max(ddp)
-
-        return max(dp)
+        # 我的第一种写法，超出时间限制
+        # n = len(nums)
+        # if n == 0: return 0  # 边界条件
+        # if n == 1: return nums[0]
+        # dp = [0] * n
+        #
+        # for i in range(1, n+1):
+        #     ddp = [0] * i
+        #     if len(range(i)) == 1: ddp[0]=nums[-1]
+        #     for j in range(i):
+        #         ddp[j] = sum(nums[i-1-j:i])
+        #     dp[i-1] = max(ddp)
+        # return max(dp)
 
 solution = Solution()
-result = solution.maxSubArray([-2,1,-3,4,-1,2,1,-5,4])
+result = solution.maxSubArray([-2,1])
 print(result)
